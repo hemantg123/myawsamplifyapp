0caf8b96133a0df6b3f6a02734571839c33aa85e
Hemant <hemant.ghagre@gmail.com>
2022-11-26 16:53:48 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to First AWS deployment.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello from Hemant.
         </a>
       </header>
     </div>
